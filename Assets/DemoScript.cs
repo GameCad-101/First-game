e37ffdf00da43ae5826d76fdeda2363b6b01a3d1
GameCad-101 <fulsherca12@uww.edu>
2026-02-16 12:10:44 -0600

    update

---
+++ Assets/DemoScript.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class DemoScript : MonoBehaviour
+{
+
+    int numLives=3;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        print(message:"Hello World");
+        print(numLives);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
